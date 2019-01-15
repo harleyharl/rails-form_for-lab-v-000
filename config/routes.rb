41808c242b706bd0bbed2937c4e8e775c1b4a59e
Harley Rayner <harleymteden@gmail.com>
2019-01-15 20:38:27 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :students, only: %i[index show new create update]
+  resources :students, only: %i[index show new create update edit]
 
   resources :school_classes, only: %i[index show new create update edit]
   #
