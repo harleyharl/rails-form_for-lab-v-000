7c675b79f79aed4fa658c165976aab6a3ca23857
Harley Rayner <harleymteden@gmail.com>
2019-01-15 20:29:35 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -29,7 +29,7 @@ class SchoolClassesController < ApplicationController
 	def update
 	  @school_class = SchoolClass.find(params[:id])
 	  @school_class.update(post_params(:title, :room_number))
-	  redirect_to school_classes_path(@post)
+	  redirect_to school_class_path(@school_class)
 	end
 
 
