f80791c9dc821f9c76834506a3d8b9e8af90a3b9
Harley Rayner <harleymteden@gmail.com>
2019-01-15 20:35:23 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,11 +16,16 @@ class StudentsController < ApplicationController
   end
 
   def create
-    # binding.pry
     @student = Student.new(student_params(:first_name, :last_name))
     @student.save
     redirect_to student_path(@student)
 	end
+
+  def update
+    @student = SchoolClass.find(params[:id])
+    @school_class.update(school_class_params(:title, :room_number))
+    redirect_to school_class_path(@school_class)
+  end
 
 
 end
